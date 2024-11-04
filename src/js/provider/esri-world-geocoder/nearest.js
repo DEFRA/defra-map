108756e62bb5650a063db432dc44b51d3e38c217
Danny Leech <dan.leech@defra.net>
2024-11-04 14:59:17 +0000

    Magic number fixes

--- src/js/provider/esri-world-geocoder/nearest.js
+++ src/js/provider/esri-world-geocoder/nearest.js
@@ -2,8 +2,8 @@ import config from './config.json'
 
 export const getNearest = async (coord, tokenCallback) => {
   const token = (await tokenCallback()).token
-  const srs = coord[0] > 1000 ? 27700 : 4326
-  coord = coord.map(n => srs === 27700 ? Math.round(n) : n)
+  const srs = coord[0] > 1000 ? config.BNG_SRS : config.WGS_84_SRS
+  coord = coord.map(n => srs === config.BNG_SRS ? Math.round(n) : n)
   const location = encodeURIComponent(`{x:${coord[0]},y:${coord[1]},spatialReference:{wkid:${srs}}}`)
   const url = config.ESRI_REVERSE_GEOCODE_URL.replace('{token}', token).replace('{location}', location)
   const response = await fetch(url)

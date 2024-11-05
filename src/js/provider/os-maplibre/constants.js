141f490126bbe3684b7a4e8b96926bc4c9393fb9
Danny Leech <dan.leech@defra.net>
2024-11-05 12:08:24 +0000

    Sonar cloud fixes

--- src/js/provider/os-maplibre/constants.js
+++ src/js/provider/os-maplibre/constants.js
@@ -10,5 +10,11 @@ export const defaults = {
   ANIMATION: {
     duration: 1000,
     curve: 0
-  }
+  },
+  DELAY: 300,
+  MAX_FEATURES: 9
+}
+
+export const css = {
+  MARKER_VISIBLE: 'fm-c-marker--visible'
 }

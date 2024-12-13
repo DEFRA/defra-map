4a37546cb234caa9c1b9dda70277576b80935797
Danny Leech <dan.leech@defra.net>
2024-12-13 14:22:33 +0000

    Complexity reduced

--- src/js/components/target.jsx
+++ src/js/components/target.jsx
@@ -7,6 +7,19 @@ const isCentre = (interfaceType, targetMarker, activePanel) => {
   return ['keyboard', 'touch'].includes(interfaceType) && !(targetMarker && activePanel === 'INFO')
 }
 
+const hasData = (isTargetCentre, features, targetMarker) => {
+  return isTargetCentre ? features?.isPixelFeaturesAtPixel : targetMarker?.hasData
+}
+
+const isVisible = (interfaceType, isTargetCentre, mode, features, targetCoord, activePanel) => {
+  let isTargetVisible = isTargetCentre && mode === 'default' && !!features ? features?.resultType === 'pixel' : !!targetCoord
+  // Hide when touch detected and a panel is displayed at the bottom
+  if (interfaceType === 'touch') {
+    isTargetVisible = isTargetVisible && (!activePanel || activePanel === 'INFO')
+  }
+  return isTargetVisible
+}
+
 export default function Target () {
   const { provider, mode, targetMarker, activePanel, viewportRef, obscurePanelRef, isContainerReady, interfaceType, isMobile } = useApp()
   const appDispatch = useApp().dispatch
@@ -14,15 +27,10 @@ export default function Target () {
   const [isObscurred] = usePixelObscurred()
 
   const isTargetCentre = isCentre(interfaceType, targetMarker, activePanel)
-  const hasTargetData = isTargetCentre ? features?.isPixelFeaturesAtPixel : targetMarker?.hasData
+  const hasTargetData = hasData(isTargetCentre, features, targetMarker)
   const targetCoord = !isTargetCentre ? targetMarker?.coord : null
-  let isTargetVisible = isTargetCentre && mode === 'default' && !!features ? features?.resultType === 'pixel' : !!targetCoord
-
-  // Hide when touch detected and a panel is displayed at the bottom
-  if (interfaceType === 'touch') {
-    isTargetVisible = isTargetVisible && (!activePanel || activePanel === 'INFO')
-  }
-
+  const isTargetVisible = isVisible(interfaceType, isTargetCentre, mode, features, targetCoord, activePanel)
+  
   // Update app state
   useEffect(() => {
     appDispatch({ type: 'SET_IS_TARGET_VISIBLE', payload: isTargetVisible })

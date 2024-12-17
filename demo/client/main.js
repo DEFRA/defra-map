0f696e28f198d9b901e6c33a681d5863a43faa4b
Danny Leech <dan.leech@defra.net>
2024-12-17 10:02:55 +0000

    Latest dependabot upgrades

--- demo/client/main.js
+++ demo/client/main.js
@@ -230,10 +230,7 @@ const fm = new FloodMap('map', {
     // hasData: true,
     width: '360px',
     label: '[dynamic title]',
-    html: `
-            <p class="govuk-body-s">[dynamic body]</p>
-            <p class="govuk-body-s">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-        `
+    html: '<p class="govuk-body-s">[dynamic body]</p>'
   },
   queryPixel: ['river-sea-fill', 'surface-water-30-fill', 'surface-water-100-fill', 'surface-water-1000-fill'],
   queryFeature: ['warning-fill', 'warning-symbol', 'stations', 'stations-small', 'five-day-forecast']
@@ -298,11 +295,12 @@ fm.addEventListener('query', e => {
     fm.info = {
       width: '360px',
       label: 'Annual likelihood of flooding',
-      html: `<div class="govuk-body-s">
-                ${location}
-                ${results}
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-            </div>`
+      html: `
+        <div class="govuk-body-s">
+          ${location}
+          ${results}
+        </div>
+      `
     }
   }
 

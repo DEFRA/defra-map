9d9b7a2c4382c31cd8a2ff9d368038e97cb82770
Dan Leech <dan.leech@defra.gov.uk>
2025-01-22 16:14:04 +0000

    test

--- demo/client/main.js
+++ demo/client/main.js
@@ -249,6 +249,9 @@ fm.addEventListener('ready', e => {
   const isSmartTV = /smart-tv|smarttv|appletv|googletv|hbbtv|netcast|tizen|webos|roku|viera|samsungtv|panasonictv|sonytv|lgnetcast|nettv/.test(userAgent)
   const dev = document.getElementById('dev')
   dev.innerHTML = `<p>${navigator.userAgent}</p><p>isSmartTV: ${isSmartTV}</p><p id="input">Input</p>`
+  window.addEventListener('keydown', e => {
+    document.getElementById('input').innerHTML = `keydown: ${e.key}, ${e.code}`
+  })
   window.addEventListener('pointermove', e => {
     document.getElementById('input').innerHTML = `pointermove: ${e.clientX}, ${e.clientY}`
   })

834a9f04a172e17039fa1df7fa2c0dee32f48f78
Ash <ashley@digicodeltd.com>
2025-03-20 12:53:02 +0000

    lib/search

--- src/js/lib/search.js
+++ src/js/lib/search.js
@@ -1,7 +1,11 @@
 export const createMessage = (suggestions, selected) => {
-  const results = `${suggestions?.length} result${suggestions?.length !== 0 ? 's' : ''} are available.`
-  let message = suggestions?.length ? results : 'No results are available.'
+  let message = 'No results are available.'
+
+  message = suggestions?.length ? `${suggestions?.length} results are available.` : message
+
   const isHighlighted = suggestions?.length && selected >= 0
+
   message = isHighlighted ? `${suggestions[selected].text}. ${selected + 1} of ${suggestions?.length} is highlighted.` : message
+
   return message
 }

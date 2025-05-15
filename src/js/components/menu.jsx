f18aae727e33241aa3d6995320ee27b7ac511b4f
Dan Leech <dan.leech@defra.gov.uk>
2025-05-15 15:27:36 +0100

    High contrast fixes

--- src/js/components/menu.jsx
+++ src/js/components/menu.jsx
@@ -35,8 +35,6 @@ export default function Menu () {
     activeRef.current?.focus()
   }
 
-  const buttonPath = drawModes.find(m => m.id === shape)?.path
-
   return (
     <div className='fm-c-menu'>
       <div className='fm-c-menu__group'>
@@ -49,7 +47,7 @@ export default function Menu () {
                   <path d='M11.298 4.666l3.536 3.536-7.071 7.071-3.536-3.536 7.071-7.071zm2.475-2.475a1.5 1.5 0 0 1 2.121 0l1.415 1.415a1.5 1.5 0 0 1 0 2.121l-1.768 1.768-3.536-3.536 1.768-1.768zM3.52 12.444l3.536 3.536-5.304 1.768 1.768-5.304z' fill='currentColor' stroke='none' />
                   )
                 : (
-                  <path d={buttonPath} />
+                  <path d={drawModes[0].path} />
                   )}
             </svg>
             <span className='fm-c-btn__label'>

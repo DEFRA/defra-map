099c4d926088ce594913c2dd6fa8cdfc98b9588f
Dan Leech <dan.leech@defra.gov.uk>
2025-07-01 10:11:44 +0100

    Reset tooltip added

--- src/js/components/reset.jsx
+++ src/js/components/reset.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useApp } from '../store/use-app'
 import { useViewport } from '../store/use-viewport'
+import Tooltip from './tooltip.jsx'
 
 export default function Reset () {
   const { options } = useApp()
+  const { id } = options
   const { bounds, oCentre, rZoom, center, zoom } = useViewport()
   const viewportDispatch = useViewport().dispatch
   const [isNewArea, setIsNewArea] = useState(false)
@@ -27,11 +29,13 @@ export default function Reset () {
   }
 
   return (
-    <button onClick={handleOnClick} className='fm-c-btn fm-c-btn--reset' aria-label='Reset map area' aria-disabled={!isNewArea} aria-controls={`${options.id}-viewport`}>
-      <svg aria-hidden='true' focusable='false' width='20' height='20' viewBox='0 0 20 20' fillRule='evenodd'>
-        <path d='M2.054 7.871L5.25 1.407l4 6.928-7.196-.464z' />
-        <path d='M7.25 4.871A6.46 6.46 0 0 1 10.5 4c3.587 0 6.5 2.913 6.5 6.5S14.087 17 10.5 17 4 14.087 4 10.5' fill='none' stroke='currentColor' strokeWidth='2' />
-      </svg>
-    </button>
+    <Tooltip id={`${id}-reset-label`} cssModifier='reset' position='left' text='Reset map view'>
+      <button onClick={handleOnClick} className='fm-c-btn fm-c-btn--reset' aria-labelledby={`${id}-reset-label`} aria-controls={`${id}-viewport`} aria-disabled={!isNewArea}>
+        <svg aria-hidden='true' focusable='false' width='20' height='20' viewBox='0 0 20 20' fillRule='evenodd'>
+          <path d='M1.554 7.371L4.75.907l4 6.928-7.196-.464z' fill='currentColor'/>
+          <path d='M6.75 4.371A6.46 6.46 0 0 1 10 3.5c3.587 0 6.5 2.913 6.5 6.5s-2.913 6.5-6.5 6.5-6.5-2.913-6.5-6.5' fill='none' stroke='currentColor' strokeWidth='2'/>
+        </svg>
+      </button>
+    </Tooltip>
   )
 }

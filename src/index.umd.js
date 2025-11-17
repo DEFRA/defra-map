10679884fdbf144778a6d62f5e3a2e0b2f46ab51
Dan Leech <dan.leech@defra.gov.uk>
2025-11-17 16:47:21 +0000

    UMD amends

--- src/index.umd.js
+++ src/index.umd.js
@@ -1,40 +1,64 @@
 // Import Preact modules - these will be bundled into the output
 import * as preact from 'preact'
-import * as preactCompat from 'preact/compat'
 import * as preactHooks from 'preact/hooks'
+import * as preactCompat from 'preact/compat'
 import * as preactJsxRuntime from 'preact/jsx-runtime'
 
-// Immediately set up window globals for plugins to use
-window.preact = preact
-window.preactHooks = preactHooks
-window.preactCompat = preactCompat
-window.preactJsxRuntime = preactJsxRuntime
+// CRITICAL FIX: The issue is that we're assigning Module objects
+// We need to create plain objects with the actual exports
 
-// Check if default exists and is a getter, if not add it
-if (!window.preactCompat.default) {
-  try {
-    window.preactCompat.default = window.preactCompat
-  } catch (e) {
-    // If it fails (readonly), it means default already exists as a getter
-    // which is fine - it should point to preactCompat anyway
-  }
+// 1. Set up base preact - expose all exports as properties
+window.preact = {
+  ...preact,
+  // Ensure these critical ones are present
+  h: preact.h,
+  render: preact.render,
+  Component: preact.Component,
+  Fragment: preact.Fragment,
+  createContext: preact.createContext,
+  createElement: preact.createElement
 }
 
-// Add React 18 createRoot compatibility if it doesn't exist
+// 2. Set up hooks - CRITICAL: spread all hooks into a plain object
+window.preactHooks = {
+  ...preactHooks,
+  // Explicitly ensure these are there
+  useState: preactHooks.useState,
+  useEffect: preactHooks.useEffect,
+  useContext: preactHooks.useContext,
+  useReducer: preactHooks.useReducer,
+  useCallback: preactHooks.useCallback,
+  useMemo: preactHooks.useMemo,
+  useRef: preactHooks.useRef,
+  useImperativeHandle: preactHooks.useImperativeHandle,
+  useLayoutEffect: preactHooks.useLayoutEffect,
+  useDebugValue: preactHooks.useDebugValue
+}
+
+// 3. Set up compat - this should include everything from preactCompat AND hooks
+window.preactCompat = {
+  ...preactCompat,
+  // CRITICAL: Also include all hooks on preactCompat for React compatibility
+  ...preactHooks,
+  // Ensure default export
+  default: preactCompat.default || preactCompat
+}
+
+// Add React 18 createRoot compatibility
 if (!window.preactCompat.createRoot) {
   window.preactCompat.createRoot = function(container) {
     return {
       render: function(vnode) {
-        window.preact.render(vnode, container)
+        preact.render(vnode, container)
       },
       unmount: function() {
-        window.preact.render(null, container)
+        preact.render(null, container)
       }
     }
   }
 }
 
-// Set up jsx runtime shim
+// 4. Set up jsx runtime
 function createJsxFunction() {
   return function(type, props, key) {
     const finalProps = props || {}
@@ -46,22 +70,34 @@ function createJsxFunction() {
     delete finalProps.children
     
     if (children !== undefined) {
-      return window.preact.h(type, finalProps, children)
+      return preact.h(type, finalProps, children)
     }
-    return window.preact.h(type, finalProps)
+    return preact.h(type, finalProps)
   }
 }
 
-const jsxRuntimeShim = {
+window.preactJsxRuntime = {
+  ...preactJsxRuntime,
   jsx: createJsxFunction(),
   jsxs: createJsxFunction(),
   jsxDEV: createJsxFunction(),
-  Fragment: window.preact.Fragment
+  Fragment: preact.Fragment
 }
-jsxRuntimeShim.default = jsxRuntimeShim
+window.preactJsxRuntime.default = window.preactJsxRuntime
+window.jsxRuntime = window.preactJsxRuntime
 
-window.preactJsxRuntime = jsxRuntimeShim
-window.jsxRuntime = jsxRuntimeShim
+// Debug: Log what's available
+console.log('Preact globals set up:', {
+  preact: !!window.preact,
+  'preact.h': typeof window.preact.h,
+  'preact.render': typeof window.preact.render,
+  preactHooks: !!window.preactHooks,
+  'preactHooks.useState': typeof window.preactHooks.useState,
+  'preactHooks.useEffect': typeof window.preactHooks.useEffect,
+  preactCompat: !!window.preactCompat,
+  'preactCompat.useState': typeof window.preactCompat.useState,
+  preactJsxRuntime: !!window.preactJsxRuntime
+})
 
 // Now import and export your library
 import DefraMap from './index.js'

9b7e359cd0dd13ae0c2fd18748d1d1df9bb85a33
Dan Leech <dan.leech@defra.gov.uk>
2025-12-01 20:52:57 +0000

    Minor UMD config amend

--- plugins/scale-bar/src/ScaleBar.jsx
+++ plugins/scale-bar/src/ScaleBar.jsx
@@ -24,15 +24,12 @@ export function ScaleBar ({
 
   return (
     <div className="dm-c-scale-bar" ref={elRef} style={{ width: `${scale.width}px` }}>
-      Test
+      <span className="dm-c-scale-bar__label">
+        <span className="dm-u-visually-hidden">Scale bar: </span>
+        {scale.label || ' '}
+        <span aria-hidden="true">{scale.abbr}</span>
+        <span className="dm-u-visually-hidden">{scale.unit}</span>
+      </span>
     </div>
-    // <div className="dm-c-scale-bar" ref={elRef} style={{ width: `${scale.width}px` }}>
-    //   <span className="dm-c-scale-bar__label">
-    //     <span className="dm-u-visually-hidden">Scale bar: </span>
-    //     {scale.label || ' '}
-    //     <span aria-hidden="true">{scale.abbr}</span>
-    //     <span className="dm-u-visually-hidden">{scale.unit}</span>
-    //   </span>
-    // </div>
   )
 }
